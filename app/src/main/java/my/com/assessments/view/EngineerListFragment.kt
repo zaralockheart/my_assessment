a9e762b2bad49dcbb33e27067cee88b8fb0c2d2f
yuzuriha <zaralockheart@gmail.com>
2020-01-28 23:57:19 +0800

    improve coroutines.

--- app/src/main/java/my/com/assessments/view/EngineerListFragment.kt
+++ app/src/main/java/my/com/assessments/view/EngineerListFragment.kt
@@ -1,17 +1,14 @@
 package my.com.assessments.view
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
 import androidx.navigation.findNavController
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import my.com.assessments.R
 import my.com.assessments.adapter.EngineerListAdapter
@@ -19,6 +16,7 @@ import my.com.assessments.databinding.EngineerListFragmentBinding
 import my.com.assessments.model.Engineer
 import my.com.assessments.utilities.Injectors
 import my.com.assessments.viewmodel.EngineerListViewModel
+import retrofit2.HttpException
 
 class EngineerListFragment : Fragment() {
 
@@ -47,15 +45,18 @@ class EngineerListFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        runBlocking {
-            try {
-                binding.isLoading = true
-                viewModel.getEngineer()
-            } catch (e: Exception) {
-                Log.e("TAG", e.message ?: "")
-            } finally {
-                binding.isLoading = false
-            }
+        try {
+            binding.isLoading = true
+            runBlocking { viewModel.getEngineer() }
+        } catch (e: HttpException) {
+            AlertDialog.Builder(context!!)
+                .setTitle(getString(R.string.error_fetching))
+                .setMessage(getString(R.string.please_check))
+                .setPositiveButton(android.R.string.yes) { dialog, _ -> dialog.cancel() }
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show()
+        } finally {
+            binding.isLoading = false
         }
         super.onViewCreated(view, savedInstanceState)
     }
--- app/src/main/java/my/com/assessments/viewmodel/EngineerListViewModel.kt
+++ app/src/main/java/my/com/assessments/viewmodel/EngineerListViewModel.kt
@@ -2,9 +2,6 @@ package my.com.assessments.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import my.com.assessments.model.Engineer
 import my.com.assessments.repository.EngineerService
 
@@ -14,10 +11,6 @@ class EngineerListViewModel internal constructor(
     val engineer: MutableLiveData<Engineer> = MutableLiveData()
 
     suspend fun getEngineer() {
-        // TODO(Yusuf): Change this to data from outside
-        if (engineer.value == null) {
-            val engineers = engineerRepository.getEngineers()
-            engineer.value = engineers
-        }
+        engineer.value = engineer.value ?: engineerRepository.getEngineers()
     }
 }
